514eda28a0724b665449756efc9fc4388d9ae1fd
wacax <wacax@hotmail.com>
2014-07-24 21:12:34 -0500

    added plots again

--- LibertyMain.R
+++ LibertyMain.R
@@ -5,8 +5,8 @@
 #Init
 rm(list=ls(all=TRUE))
 
-#Load/install libraries
-
+#Load/install required libraries
+require('ggplot2')
 
 #Set Working Directory
 workingDirectory <- "/home/wacax/Wacax/Kaggle/Liberty Mutual Group/Liberty Mutual Group - Fire Peril Loss Cost"
@@ -20,11 +20,17 @@ dataDirectory <- '/home/wacax/Wacax/Kaggle/Liberty Mutual Group/Data/'
 #############################
 #Load Data
 #Input Data
-train <- read.csv(paste0(dataDirectory, 'train.csv'), header = TRUE, stringsAsFactors = FALSE)
-test <- read.csv(paste0(dataDirectory, 'test.csv'), header = TRUE, stringsAsFactors = FALSE)
+rows2read <- 20000
+train <- read.csv(paste0(dataDirectory, 'train.csv'), header = TRUE, stringsAsFactors = FALSE, nrows = rows2read)
+test <- read.csv(paste0(dataDirectory, 'test.csv'), header = TRUE, stringsAsFactors = FALSE, nrows = rows2read)
 
 submissionTemplate <- read.csv(paste0(dataDirectory, 'sampleSubmission.csv'), header = TRUE, stringsAsFactors = FALSE)
 
 ###############################
 #EDA
-names(train)
+str(train)
+fireProb <- table(ifelse(train$target > 0, 1, 0)) / length(ifelse(train$target > 0, 1, 0))
+fireCosts <- as.data.frame(train$target[train$target>0]); names(fireCosts) <- 'Cost'
+ggplot(data = train, aes(x = ifelse(train$target > 0, TRUE, FALSE))) +  geom_histogram() 
+ggplot(data = fireCosts, aes(x = Cost)) +  geom_density() 
+ggplot(data = fireCosts, aes(x = log(Cost))) +  geom_density() 
