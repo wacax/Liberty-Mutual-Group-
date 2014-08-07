a96bbaacec0ae1c2c826ce7fc4a10c30d89d6a91
wacax <wacax@hotmail.com>
2014-08-07 16:47:21 -0500

    added weights on regsubsets and gbms for feature selection

--- LibertyMain.R
+++ LibertyMain.R
@@ -1,5 +1,5 @@
 #Liberty Mutual Group - Fire Peril Loss Cost
-#ver 0.5
+#ver 0.8
 
 #########################
 #Init
@@ -29,7 +29,7 @@ source(paste0(workingDirectory, 'WeightedGini.R'))
 #############################
 #Load Data
 #Input Data
-rows2read <- 1000
+rows2read <- 'all'
 train <- read.csv(paste0(dataDirectory, 'train.csv'), header = TRUE, stringsAsFactors = FALSE, nrows = ifelse(class(rows2read) == 'character', -1, rows2read))
 test <- read.csv(paste0(dataDirectory, 'test.csv'), header = TRUE, stringsAsFactors = FALSE, nrows = ifelse(class(rows2read) == 'character', -1, rows2read))
 
@@ -40,6 +40,9 @@ submissionTemplate <- read.csv(paste0(dataDirectory, 'sampleSubmission.csv'), he
 #extract gini weights
 weightsTrain <- train$var11
 weightsTest <- test$var11
+
+#NA indices, regsubsets and kmeans are sensitive to NAs
+noNAIndices <- which(apply(is.na(train), 1, sum) == 0)
 
 #Add a new column that indicates whether there was a fire or not
 train['fire'] <- ifelse(train$target > 0, 1, 0)
@@ -62,9 +65,6 @@ ggplot(data = train, aes(x = ifelse(train$target > 0, TRUE, FALSE))) +  geom_his
 ggplot(data = fireCosts, aes(x = Cost)) +  geom_density() 
 ggplot(data = fireCosts, aes(x = log(Cost))) +  geom_density() 
 
-#NA omit, regsubsets and kmeans are sensitive to NAs
-noNAIndices <- which(apply(is.na(train), 1, sum) == 0)
-
 #Clustering
 #Kmeans (2 groups), The idea is to see if kmeans clustering can help explore the 
 #fire vs no fire groups and if they match to some extent to the given labels
@@ -77,22 +77,24 @@ derp <- princomp(train[noNAIndices, c('fire', 'weatherVar32', 'weatherVar33')])
 #Predictors Selection
 #Linear Feature Selection
 #Fire or No-Fire Predictors
-predictors1 <- linearFeatureSelection(fire ~ ., allPredictorsData = train[, c(seq(3, 19), seq(21,303))])
+predictors1 <- linearFeatureSelection(fire ~ ., train[, c(seq(3, 19), seq(21,303))])
 predictors1 <- predictors1[[1]]
 #Predictor selection using trees
-treeModel <- gbm.fit(train[, c(seq(3, 19), seq(21,302))], as.factor(train$fire), distribution = 'bernoulli', nTrain = floor(nrow(train) *0.7), n.trees = 2500)
+treeModel <- gbm(fire ~ ., train[, c(seq(3, 19), seq(21,303))], distribution = 'adaboost',
+                 train.fraction = 0.7, n.trees = 1000, weights = weightsTrain)
 best.iter <- gbm.perf(treeModel, method="test")
 GBMClassPredictors <- summary(treeModel)
 GBMClassPredictors <- as.character(GBMClassPredictors$var[GBMClassPredictors$rel.inf > 1])
 predictors1 <- union(predictors1, GBMClassPredictors)
 
 #Fire damage regression predictor
-
 whichFire <- which(train$target > 0)
-predictorsRegression <- linearFeatureSelection(target ~ ., allPredictorsData = train[whichFire, c(seq(2, 19), seq(21,302))], userMax = 100)
+noNAIndices <- intersect(noNAIndices, whichFire)
+predictorsRegression <- linearFeatureSelection(target ~ ., train[whichFire, c(seq(2, 19), seq(21,302))], userMax = 100)
 predictorsRegression <- predictorsRegression[[1]]
 #Predictor selection using trees
-treeModel <- gbm.fit(train[whichFire, c(seq(3, 19), seq(21,302))], train$target[whichFire], distribution = 'gaussian', nTrain = floor(length(whichFire) *0.7), n.trees = 4000)
+treeModel <- gbm(target ~ ., train[whichFire, c(seq(2, 19), seq(21,302))], distribution = 'gaussian', 
+                 train.fraction = 0.7, n.trees = 1000, weights = weightsTrain[whichFire])
 best.iter <- gbm.perf(treeModel, method="test")
 GBMGregPredictors <- summary(treeModel)
 GBMGregPredictors <- as.character(GBMGregPredictors$var[GBMGregPredictors$rel.inf > 1])
@@ -124,10 +126,12 @@ rmse.cv=sqrt(apply(cv.errors,2,mean))
 plot(rmse.cv,pch=19,type="b")
 
 #All Data Fire Damage Regression
-predictorsAllData <- linearFeatureSelection(target ~ ., allPredictorsData = train[, c(seq(2, 19), seq(21,302))], userMax = 100)
+noNAIndices <- which(apply(is.na(train), 1, sum) == 0)
+predictorsAllData <- linearFeatureSelection(target ~ ., train[, c(seq(2, 19), seq(21,302))], userMax = 100)
 predictorsAllData <- predictorsAllData[[1]]
 #Predictor selection using trees
-treeModel <- gbm.fit(train[, c(seq(3, 19), seq(21,302))], train$target, distribution = 'gaussian', nTrain = floor(nrow(train) *0.7), n.trees = 500)
+treeModel <- gbm(target ~ ., train[, c(seq(2, 19), seq(21,302))], distribution = 'gaussian', 
+                 train.fraction = 0.7, n.trees = 1000, weights = weightsTrain)
 best.iter <- gbm.perf(treeModel, method="test")
 GBMAllPredictors <- summary(treeModel)
 GBMAllPredictors <- as.character(GBMAllPredictors$var[GBMAllPredictors$rel.inf > 1])
@@ -153,14 +157,6 @@ plot(rmse.cv,pch=19,type="b")
 #MODELLING
 #GBM
 #Cross-validation
-GBMModel <- gbm.fit(x = train[ , predictors1], y = train$fire, n.trees = 1000,
-                    interaction.depth = 4, verbose = TRUE, nTrain = floor(nrow(train) * 0.7), distribution = 'bernoulli')
-#Competition Scores
-NormalizedWeightedGini <- function(solution, weights, submission) {
-  WeightedGini(solution, weights, submission) / WeightedGini(solution, weights, solution)
-}
-
-#Cross-validation
 #Add a new column loss or not as factor
 train['lossFactor'] <- as.factor(ifelse(train$target > 0, 1, 0))
 
@@ -170,7 +166,7 @@ GBMControl <- trainControl(method = "cv",
 
 gbmGrid <- expand.grid(.interaction.depth = seq(1, 5, 2),
                        .shrinkage = c(0.001, 0.003, 0.01), 
-                       .n.trees = 1000)
+                       .n.trees = 2500)
 
 gbmMODClass <- train(form = lossFactor ~ ., 
                      data = train[ , c(predictors1, 'lossFactor')],
@@ -182,12 +178,7 @@ gbmMODClass <- train(form = lossFactor ~ .,
                      verbose = TRUE)
 
 #Best Number of trees
-GBMModel <- gbm.fit(x = train[ , predictors1], y = train$fire, distribution = 'bernoulli',
-                    interaction.depth = gbmMODClass$bestTune[2], shrinkage = gbmMODClass$bestTune[3], 
-                    n.trees = 2500, nTrain = floor(nrow(train) * 0.7),
-                    verbose = TRUE)
-
-treesClass <- gbm.perf(GBMModel,method = 'test')
+treesClass <- gbm.perf(gbmMODClass$finalModel, method = 'test')
 
 #Final Model
 #Fire - No Fire Model
@@ -196,17 +187,18 @@ GBMModel <- gbm.fit(x = train[ , predictors1], y = train$fire, distribution = 'b
                     n.trees = treesClass, verbose = TRUE)
 summary.gbm(GBMModel)
 plot.gbm(GBMModel)
-pretty.gbm.tree(GBMModel, i.tree = 1000)
+pretty.gbm.tree(GBMModel, i.tree = treesClass)
 
 #Value Regression
-#5 Fold Cross-Validation + best distribution
+#5 Fold Cross-Validation 
+whichFire <- which(train$target > 0)
 GBMControl <- trainControl(method="cv",
                            number=5,
                            verboseIter=TRUE)
 
 gbmGrid <- expand.grid(.interaction.depth = seq(1, 7, 3),
                        .shrinkage = c(0.001, 0.003, 0.01), 
-                       .n.trees = 1000)
+                       .n.trees = 5000)
 
 gbmMODReg <- train(form = target ~ ., 
                    data = train[whichFire , c(predictorsRegression, 'target')],
@@ -218,21 +210,14 @@ gbmMODReg <- train(form = target ~ .,
                    verbose = TRUE)
 
 #Best Number of trees
-whichFire <- which(train$target > 0)
-GBMModelReg <- gbm.fit(x = train[whichFire , predictorsRegression], y = train$target[whichFire], 
-                       distribution = 'gaussian', interaction.depth = gbmMODReg$bestTune[2], 
-                       shrinkage = gbmMODReg$bestTune[3], nTrain = floor(nrow(train) * 0.7),
-                       n.trees = 4000, verbose = TRUE)
-
-treesReg <- gbm.perf(GBMModelReg, method = 'test')
-
+treesReg <- gbm.perf(gbmMODReg$finalModel, method = 'test')
 #Final Model
 GBMModelReg <- gbm.fit(x = train[whichFire , predictorsRegression], y = train$target[whichFire], 
                        distribution = 'gaussian', interaction.depth = gbmMODReg$bestTune[2], 
                        shrinkage = gbmMODReg$bestTune[3], n.trees = treesReg, verbose = TRUE)
 summary.gbm(GBMModelReg)
 plot.gbm(GBMModelReg)
-pretty.gbm.tree(GBMModelReg, i.tree = 1000)
+pretty.gbm.tree(GBMModelReg, i.tree = treesReg)
 
 #Full Data Value Regression
 #5 Fold Cross-Validation + best distribution
@@ -242,7 +227,7 @@ GBMControl <- trainControl(method="cv",
 
 gbmGrid <- expand.grid(.interaction.depth = seq(1, 7, 3),
                        .shrinkage = c(0.001, 0.003, 0.01), 
-                       .n.trees = 1000)
+                       .n.trees = 2500)
 
 gbmMODAll <- train(form = target ~ ., 
                    data = train[ , c(predictorsAllData, 'target')],
@@ -254,11 +239,7 @@ gbmMODAll <- train(form = target ~ .,
                    verbose = TRUE)
 
 #Best Number of trees
-GBMModelAll <- gbm.fit(x = train[ , predictorsAllData], y = train$target, 
-                       distribution = 'gaussian', interaction.depth = gbmMODAll$bestTune[2], 
-                       shrinkage = gbmMODAll$bestTune[3], n.trees = 4000, nTrain = floor(nrow(train) * 0.7), verbose = TRUE)
-
-treesAll <- gbm.perf(GBMModelAll,method = 'test')
+treesAll <- gbm.perf(gbmMODAll$finalModel, method = 'test')
 
 #Final Model
 GBMModelAll <- gbm.fit(x = train[ , predictorsAllData], y = train$target, 
@@ -266,7 +247,48 @@ GBMModelAll <- gbm.fit(x = train[ , predictorsAllData], y = train$target,
                        shrinkage = gbmMODAll$bestTune[3], n.trees = treesAll, verbose = TRUE)
 summary.gbm(GBMModelAll)
 plot.gbm(GBMModelAll)
-pretty.gbm.tree(GBMModelAll, i.tree = 1000)
+pretty.gbm.tree(GBMModelAll, i.tree = treesAll)
+
+#Ensemble of Models
+trainGBMPred <- predict(GBMModel, newdata = train[ , predictors1], n.trees = treesClass, type = 'response')
+trainGBMReg <- predict(GBMModelReg, newdata = train[ , predictorsRegression], n.trees = treesReg)
+trainGBMAll <- predict(GBMModelAll, newdata = train[ , predictorsAllData], n.trees = treesAll)
+
+#train responses data frame
+GBMTrainPredictionsTwoPredictors <- data.frame(trainGBMPred, trainGBMReg, train$target)
+names(GBMTrainPredictionsTwoPredictors)[3] <- 'target'
+GBMTrainPredictionsFull <- data.frame(trainGBMPred, trainGBMReg, trainGBMAll, train$target)
+
+#Build Ensembles
+GBMControl <- trainControl(method="cv",
+                           number=5,
+                           verboseIter=TRUE)
+
+gbmGrid <- expand.grid(.interaction.depth = seq(1, 7, 3),
+                       .shrinkage = c(0.001, 0.003, 0.01), 
+                       .n.trees = 3500)
+
+gbmMODEnsembleTwo <- train(form = target ~ ., 
+                           data = GBMTrainPredictionsTwoPredictors,
+                           method = "gbm",
+                           tuneGrid = gbmGrid,
+                           trControl = GBMControl,
+                           distribution = 'gaussian',
+                           train.fraction = 0.7,
+                           verbose = TRUE)
+
+#Best Number of trees
+treesEnsembleTwo <- gbm.perf(gbmMODEnsembleTwo$finalModel, method = 'test')
+
+GBMEnsembleTwo <- gbm.fit(x = GBMTrainPredictionsTwoPredictors[, c('trainGBMPred', 'trainGBMReg')],
+                          y = GBMTrainPredictionsTwoPredictors$target,
+                          distribution = 'gaussian', interaction.depth = gbmMODEnsembleTwo$bestTune[2],
+                          shrinkage = gbmMODEnsembleTwo$bestTune[3],
+                          n.trees = treesEnsembleTwo, verbose = TRUE)
+
+GBMEnsembleFull <- gbm.fit(x = GBMTrainPredictionsFull[, c('trainGBMPred', 'trainGBMReg', 'trainGBMAll')],
+                           y = GBMTrainPredictionsFull$target, distribution = 'gaussian', 
+                           interaction.depth = 1, shrinkage = 0.001, n.trees = 3000, verbose = TRUE)
 
 #GLM 
 #Cross-Validaton
@@ -300,7 +322,6 @@ GLMNETModel <- GLMNETModelCV$glmnet.fit
 plot(GLMNETModel, xvar="lambda", label=TRUE)
 
 #Regression
-
 #Cross-validation
 #Find regression targets
 whichFire <- which(train$target > 0)
@@ -320,6 +341,30 @@ coef(GLMNETModelCV)
 GLMNETModelReg <- GLMNETModelCVReg$glmnet.fit
 plot(GLMNETModelReg, xvar="lambda", label=TRUE)
 
+#All Data
+#Cross-validation
+#transform train Dataframe to model matrix as glmnet only accepts matrices as input
+trainAllMatrix <- model.matrix(~ . , data = train[ , c(predictorsAllData, 'target')]) 
+#cross validate the data, glmnet does it automatially there is no need for the caret package or a custom CV
+GLMNETModelCVAll <- cv.glmnet(x = trainAllMatrix[,1:dim(trainAllMatrix)[2]-1], y = trainAllMatrix[,dim(trainAllMatrix)[2]], nfolds = 5, parallel = TRUE, family = 'gaussian')
+plot(GLMNETModelCVAll)
+coef(GLMNETModelCVAll)
+
+#Final Model
+#this is not recommended by the package authors, use GLMNETModelCVAll$glmnet.fit instead
+#GLMNETModel <- glmnet(x = train[,1:dim(train)[2]-1], y = train[,dim(train)[2]], family = 'binomial', lamda = GLMNETModelCV$lambda.min) 
+#plot(GLMNETModel, xvar="lambda", label=TRUE)
+
+#Recommended use
+GLMNETModelAll <- GLMNETModelCVAll$glmnet.fit
+plot(GLMNETModelAll, xvar="lambda", label=TRUE)
+
+##########################################################
+#Competition Scores
+NormalizedWeightedGini <- function(solution, weights, submission) {
+  WeightedGini(solution, weights, submission) / WeightedGini(solution, weights, solution)
+}
+
 ##########################################################
 #PREDICTIONS
 #GBM
@@ -329,6 +374,22 @@ GBMPrediction <- predict(GBMModel, newdata = test[ , predictors1], n.trees = tre
 GBMPredictionReg <- predict(GBMModelReg, newdata = test[ , predictorsRegression], n.trees = treesReg)
 #All Data Regression Prediction
 GBMPredictionAll <- predict(GBMModelAll, newdata = test[ , predictorsAllData], n.trees = treesAll)
+
+#2-3 models Ensembles
+#Simple combinations
+#Classification - Regression
+GBMPredReg <- GBMPrediction * GBMPredictionReg
+#Classification - All
+GBMPredAll <- GBMPrediction * GBMPredictionAll
+#Regression - All
+GBMPredAll <- GBMPredictionReg * GBMPredictionAll
+
+#GBM of ensemble of Models, the final result will be compared to the targets columns in the train dataframe data
+GBMTestPredictions <- data.frame(cbind(GBMPrediction, GBMPredictionReg, GBMPredictionAll))
+
+ensamblePredictions <- predict(GBMEnsemble, newdata = GBMTestPredictions, n.trees = 3000)
+
+
 
 #GLM
 #this removes the "factor var4 has new levels A1, Z" error
@@ -356,6 +417,10 @@ GBMPrediction[-fireIndices$ix[1:floor(length(GBMPrediction) * 0.03)]] <- 0
 
 
 #########################################################
-#Write .csv
-submissionTemplate$target <- GBMPredictionAll
-write.csv(submissionTemplate, file = "predictionTestIII.csv", row.names = FALSE)
+#Write .csv multiplication
+submissionTemplate$target <- GBMPredReg
+write.csv(submissionTemplate, file = "predictionI.csv", row.names = FALSE)
+
+#Write .csv ensemble
+submissionTemplate$target <- ensamblePredictions
+write.csv(submissionTemplate, file = "predictionTestII.csv", row.names = FALSE)
--- linearFeatureSelection.R
+++ linearFeatureSelection.R
@@ -1,29 +1,29 @@
-linearFeatureSelection <- function(uformula, allPredictorsData, userMethod = 'forward', 
+linearFeatureSelection <- function(uformula, allPredictorsData, userMethod = 'forward',
                                    plotIt = TRUE, userMax = 200){
   #Linear Feature Selection Using the Leaps Package
   #TODO: Add documentation
-  
-  #NA omit, regsubsets and kmeans are sensitive to NAs
+  #NA omit, regsubsets and kmeans are sensitive to NA 
   noNAIndices <- which(apply(is.na(allPredictorsData), 1, sum) == 0)
   
   #Fire or not
-  linearBestModels <- regsubsets(uformula, data = allPredictorsData[noNAIndices, ], method = userMethod, 
-                                 nvmax = userMax, really.big = ifelse(userMethod == 'exhaustive' & ncol(allPredictorsData) > 50, TRUE, FALSE))
+  linearBestModels <- regsubsets(x = uformula,
+                                 data = allPredictorsData[noNAIndices, ], 
+                                 method = userMethod,
+                                 weights = weightsTrain[noNAIndices],
+                                 nvmax = userMax,
+                                 really.big = ifelse(userMethod == 'exhaustive' & ncol(allPredictorsData) > 50, TRUE, FALSE))
   
   bestMods <- summary(linearBestModels)
   names(bestMods)
   bestNumberOfPredictors <- which.min(bestMods$cp)
-  
   if(plotIt == TRUE){
     #ggplot of optimal number of predictors
     #ErrorsFireClasif <- as.data.frame(bestMods$cp); names(ErrorsFireClasif) <- 'CPError'
-    #print(ggplot(data = ErrorsFireClasif, aes(x = seq(1, nrow(ErrorsFireClasif)), y = CPError)) +  geom_line() +  geom_point())    
-    
+    #print(ggplot(data = ErrorsFireClasif, aes(x = seq(1, nrow(ErrorsFireClasif)), y = CPError)) + geom_line() + geom_point())
     #Regular plot of optimal number of predictors
     plot(bestMods$cp, xlab="Number of Variables", ylab="CP Error")
     points(bestNumberOfPredictors, bestMods$cp[bestNumberOfPredictors],pch=20,col="red")
   }
-
   #Extract the name of the most predictive columns' names
   #TODO: change the hard coded [2:82] that defines factors, find another way to pick factors' names
   predictors1 <- as.data.frame(bestMods$which)
